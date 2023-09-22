b942b815a6b0c61e05a6db804bb9e92755a106a3
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2023-09-22 12:31:20 -0700

    reduced levels to 6 and increased mcts to 35o nodes

--- src/scripts/run_with_args.py
+++ src/scripts/run_with_args.py
@@ -124,7 +124,7 @@ def main(args, policy_string):
                     discount_factor=0.9,
                 )
                 tree.start_timer()
-                max_steps = 300
+                max_steps = 350
                 if len(data_list) == idx:
                     data_list.append(None)
                     error = None
@@ -171,7 +171,7 @@ def main(args, policy_string):
                     num_keep=6,
                 )
                 tree.start_timer()
-                num_levels = 7
+                num_levels = 6
                 if len(data_list) == idx:
                     data_list.append(None)
                     error = None
