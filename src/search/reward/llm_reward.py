2739e4ca0d197f88bd8b9f01c9511c3d3219a71b
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2023-10-09 14:11:37 -0700

    make llm_reward vector

--- src/search/reward/llm_reward.py
+++ src/search/reward/llm_reward.py
@@ -6,7 +6,7 @@ from llm import query  # noqa: E402
 
 
 def llm_adsorption_energy_reward(
-    s: query.QueryState,
+    states: list[query.QueryState],
     reward_limit: float = 10.0,
     max_attempts: int = 3,
     primary_reward: bool = True,
@@ -14,20 +14,22 @@ def llm_adsorption_energy_reward(
     """Reward function to return adsorption energy of reactants.
 
     The calculated reward is stored in state.reward."""
+    rewards = []
+    for s in states:
+        e = reward_limit + 1
+        attempts = 0
+        while e > reward_limit and attempts < max_attempts:
+            e = s.query_adsorption_energy_list(allow_requery=primary_reward)
+            attempts += 1
 
-    e = reward_limit + 1
-    attempts = 0
-    while e > reward_limit and attempts < max_attempts:
-        e = s.query_adsorption_energy_list(allow_requery=primary_reward)
-        attempts += 1
+        if attempts == max_attempts:
+            e = -10
 
-    if attempts == max_attempts:
-        s.set_reward(-10)
-        return -10
+        if primary_reward:
+            s.set_reward(e, info_field="llm-reward")
+        else:
+            s.set_reward(e, primary_reward=primary_reward, info_field="llm-reward")
 
-    if primary_reward:
-        s.set_reward(e, info_field="llm-reward")
-    else:
-        s.set_reward(e, primary_reward=primary_reward, info_field="llm-reward")
+        rewards.append(e)
 
-    return e
+    return rewards
