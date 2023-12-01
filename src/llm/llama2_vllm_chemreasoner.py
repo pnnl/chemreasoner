abbefa0463bc8b76caca9698d1f559f598cc6414
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2023-12-01 15:55:15 -0800

    create funciton in the class for running llama

--- src/llm/llama2_vllm_chemreasoner.py
+++ src/llm/llama2_vllm_chemreasoner.py
@@ -14,9 +14,7 @@ from vllm import LLM, SamplingParams
 
 
 def init_llama(model_dir, num_gpus, **kwargs_sampling_params):
-    """
-    Uses vllm's interface to load large models over multiple GPU's
-    """
+    """Use vllm's interface to load large models over multiple GPU's"""
     # login('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')
     sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
     llm = LLM(
@@ -31,17 +29,87 @@ def init_llama(model_dir, num_gpus, **kwargs_sampling_params):
 class LlamaLLM:
     """A class to handle initializing llama."""
 
-    def __init__(self, model_dir: Path, num_gpus: int, sampling_params={}):
+    default_system_prompt = ""
+
+    def __init__(
+        self,
+        model_dir: Path,
+        num_gpus: int,
+        batch_size: int = -1,
+        sampling_args: dict = {},
+    ):
         """Load the model from disk and store it in this class."""
         # Apply default sampling params
-        sampling_params["temperature"] = sampling_params.get("temperature", 0.8)
-        sampling_params["top_p"] = sampling_params.get("top_p", 0.95)
-        sampling_params["max-tokens"] = sampling_params.get("max_tokens", 1000)
+        sampling_args["temperature"] = sampling_args.get("temperature", 0.8)
+        sampling_args["top_p"] = sampling_args.get("top_p", 0.95)
+        sampling_args["max-tokens"] = sampling_args.get("max_tokens", 1000)
 
         self.model_dir = model_dir
         self.num_gpus = num_gpus
         self.llm, self.sampling_params = init_llama(
-            self.model_dir, self.num_gpus, **sampling_params
+            self.model_dir, self.num_gpus, **sampling_args
+        )
+
+    def __call__(
+        self,
+        prompts: list[str],
+        system_prompts: list[str] = None,
+        batch_size: int = None,
+        sampling_args: dict = None,
+    ):
+        """Generate responses for the given prompts."""
+        if sampling_args is None:
+            sampling_params = self.sampling_params
+        else:
+            sampling_args["temperature"] = sampling_args.get("temperature", 0.8)
+            sampling_args["top_p"] = sampling_args.get("top_p", 0.95)
+            sampling_args["max-tokens"] = sampling_args.get("max_tokens", 1000)
+            sampling_params = SamplingParams(sampling_args)
+
+        batch_size = batch_size if batch_size is not None else self.batch_size
+        if batch_size is None:
+            batch_size = self.batch_size
+
+        processed_prompts = [
+            self.process_prompt(p, s) for p, s in zip(prompts, system_prompts)
+        ]
+
+        return self.run_llama(processed_prompts, sampling_params, batch_size)
+
+    def run_llama(
+        self,
+        processed_prompts: list[str],
+        sampling_params: SamplingParams,
+        batch_size: int,
+    ):
+        """Run the llama generation on the given processed prompts."""
+        batch_size = batch_size if batch_size is None else self.batch_size
+        batch_size = len(processed_prompts) if batch_size < 1 else batch_size
+
+        answers = []
+        for i in range(0, len(processed_prompts), batch_size):
+            print("running prompts ", i, i + batch_size)
+            batch_prompts = [
+                generate_prompt(x)
+                for x in processed_prompts[i : i + batch_size]  # noqa
+            ]
+            answers = self.llm.generate(batch_prompts, sampling_params)
+            for output in answers:
+                generated_text = output.outputs[0].text
+                answers.append(generated_text)
+
+    def process_prompt(self, prompt: str, system_prompt: str = None):
+        """Put the prompt and system prompt together."""
+        sys_prompt = (
+            system_prompt if system_prompt is not None else self.default_system_prompt
+        )
+        return (
+            "<s>[INST] <<SYS>>\n"
+            + sys_prompt
+            + "\n<</SYS>>"
+            + "\n\n"
+            + prompt
+            + " [/INST]"
         )
 
 
@@ -62,7 +130,7 @@ def generate_prompt(prompt):
 def run_llama(model_dir, outpath, num_gpus, batch_size):
     llm, sampling_params = init_llama(num_gpus, model_dir)
     prompts = json.load(open("/people/spru445/json_database.json", "r"))
-    # print(prompts)
+
     for i in range(0, len(prompts), batch_size):
         print("running prompts ", i, i + batch_size)
         batch_prompts = [generate_prompt(x) for x in prompts[i : i + batch_size]]
@@ -71,8 +139,6 @@ def run_llama(model_dir, outpath, num_gpus, batch_size):
         for output in answers:
             generated_text = output.outputs[0].text
             prompts[i]["generation_prompt"]["llama_answer"] = generated_text
-    with open(outpath, "w") as outfile:
-        json.dump(prompts, outfile)
 
 
 if __name__ == "__main__":
