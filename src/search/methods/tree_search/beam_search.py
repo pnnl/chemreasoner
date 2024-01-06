2f8761354ea2367a45e5af49a3cdb9f9b97c4cf7
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-05 16:58:37 -0800

    fixed these_priors

--- src/search/methods/tree_search/beam_search.py
+++ src/search/methods/tree_search/beam_search.py
@@ -39,8 +39,7 @@ class BeamSearchTree:
         new_nodes = []
         parent_idx = []
         for i, node in enumerate(nodes):
-            actions, priors = actions_priors[i]
-            these_priors = priors[i]
+            actions, these_priors = actions_priors[i]
             shuffle_idx = list(range(len(these_priors)))
             shuffle(shuffle_idx)
             these_priors = [these_priors[i] for i in shuffle_idx]
