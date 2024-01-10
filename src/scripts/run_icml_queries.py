6348d3dd7cd9451221e91a16ecb774dea22696ab
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-10 12:32:39 -0800

    make message more informative

--- src/scripts/run_icml_queries.py
+++ src/scripts/run_icml_queries.py
@@ -215,7 +215,9 @@ if __name__ == "__main__":
     indeces = get_indeces(args)
 
     for i in indeces:
-        logging.info(f"Processing query {i}")
+        logging.info(
+            f"=============Processing query {i}/{len(indeces)}================"
+        )
         fname = save_dir / f"test_tree_{i}.json"
         starting_state = get_state_from_idx(i, df)
 
