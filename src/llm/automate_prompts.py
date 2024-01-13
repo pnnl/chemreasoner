38afbaa7eac7bab255ce279adcfd242f2396c625
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-12 18:36:04 -0800

    add preference for water

--- src/llm/automate_prompts.py
+++ src/llm/automate_prompts.py
@@ -146,7 +146,7 @@ def get_initial_state_rwgs(
     if len(ads_symbols) == 0:
         # Do the reaction
         ads_symbols = ["*CO", "CO2", "H2", "*OH2"]
-        ads_preference = [-1, 1, 1]
+        ads_preference = [-1, 1, 1, -1]
 
     qs = ReasonerState(
         template=template,
