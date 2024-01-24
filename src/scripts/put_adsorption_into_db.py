dda4fee4785a95c293a01808df1fa6000eab20b6
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-23 22:15:57 -0800

    use /dev/shm in chemreasoner catalysis

--- src/scripts/put_adsorption_into_db.py
+++ src/scripts/put_adsorption_into_db.py
@@ -9,14 +9,14 @@ import redis
 
 redis_db = redis.Redis(host='localhost', port=6379, db=1)
 
-for p in tqdm(Path("/qfs/projects/aqe_tec4/catalysis/chemreasoner/").rglob("*/adsorption.json")):
+for p in tqdm(Path("/dev/shm/chemreasoner/catalysis").rglob("*/adsorption.json")):
     
     with open(str(p), "r") as f:
         data = json.load(f)
 
     redis_db.set(str(p), json.dumps(data))
 
-for p in tqdm(Path("/qfs/projects/aqe_tec4/catalysis/chemreasoner/catalysis", "slabs").rglob("*.pkl")):
+for p in tqdm(Path("/dev/shm/chemreasoner/catalysis", "slabs").rglob("*.pkl")):
     
     with open(str(p), "rb") as f:
         data = pickle.load(f)
