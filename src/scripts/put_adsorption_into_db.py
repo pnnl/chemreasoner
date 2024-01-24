a97fb29a1acbbd4341c5d5b66336c589f126c62a
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-23 19:47:09 -0800

    add script for saving adsorption calculations in database

---
+++ src/scripts/put_adsorption_into_db.py
@@ -0,0 +1,17 @@
+"""Save all adsorption energy results into a db."""
+import json
+
+from pathlib import Path
+from tqdm import tqdm
+
+import redis
+
+redis_db = redis.Redis(host='localhost', port=6379, db=0)
+
+for p in tqdm(Path("/qfs/projects/aqe_tec4/catalysis/chemreasoner/").rglob("*/adsorption.json")):
+    print(p)
+    
+    with open(str(p), "r") as f:
+        data = json.load(f)
+
+    redis_db.set(str(p), json.dumps(data))
