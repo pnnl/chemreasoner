1dde471edec4e0c95ffea774d2c85df91cdee29d
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-24 13:41:06 -0800

    set filter based on particular adsorption energies being large

--- src/scripts/tree_to_trace.py
+++ src/scripts/tree_to_trace.py
@@ -8,6 +8,23 @@ from pathlib import Path
 import networkx as nx
 import numpy as np
 
+def get_max_adsorption_energies(node):
+    if "simulation-reward" in node["info"].keys():
+        reward_values = node["info"]["simulation-reward"]["reward_values"]
+        numbers = []
+        for cat, v in reward_values.items():
+            print(cat)
+            for ads, values in v.items():
+                print(ads)
+                numbers.append(min(values))
+        if len(numbers) > 0:
+            return max(numbers)
+        else:
+            return node["node_rewards"]
+    else:
+        return node["node_rewards"]
+    
+
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--processed-dir", type=str)
@@ -35,10 +52,10 @@ for p in processed_dir.rglob("*.json"):
     graph = nx.readwrite.json_graph.tree_graph(data)
 
     max_idx = np.argmax(
-        [graph.nodes(data=True)[i]["node_rewards"] if graph.nodes(data=True)[i]["node_rewards"] < max_reward else -np.inf for i in range(len(graph.nodes))]
+        [graph.nodes(data=True)[i]["node_rewards"] if get_max_adsorption_energies(graph.nodes(data=True)[i]) < max_reward else -np.inf for i in range(len(graph.nodes))]
     )
     print([graph.nodes(data=True)[i]["node_rewards"] for i in range(len(graph.nodes))][max_idx])
-    filtered_list = [(i, graph.nodes(data=True)[i]["node_rewards"]) for i in range(len(graph.nodes)) if graph.nodes(data=True)[i]["node_rewards"] < max_reward ]
+    filtered_list = [(i, graph.nodes(data=True)[i]["node_rewards"]) for i in range(len(graph.nodes)) if get_max_adsorption_energies(graph.nodes(data=True)[i]) < max_reward]
     
     if len(filtered_list) > 0:
         print(f"\nFiltered out nodes: {filtered_list}\n")
