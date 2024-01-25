70c75a93d5b69a3612d7468860c77539289e7d5f
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-25 15:51:03 -0800

    test llama

--- src/llm/llama2_vllm_chemreasoner.py
+++ src/llm/llama2_vllm_chemreasoner.py
@@ -40,7 +40,6 @@ class LlamaLLM:
         self,
         model_dir: Path,
         num_gpus: int,
-        batch_size: int = -1,
         sampling_args: dict = {},
     ):
         """Load the model from disk and store it in this class."""
@@ -71,10 +70,6 @@ class LlamaLLM:
             sampling_args["max-tokens"] = sampling_args.get("max_tokens", 1000)
             sampling_params = SamplingParams(sampling_args)
 
-        batch_size = batch_size if batch_size is not None else self.batch_size
-        if batch_size is None:
-            batch_size = self.batch_size
-
         processed_prompts = [
             self.process_prompt(p, s) for p, s in zip(prompts, system_prompts)
         ]
@@ -88,11 +83,9 @@ class LlamaLLM:
         batch_size: int,
     ):
         """Run the llama generation on the given processed prompts."""
-        batch_size = batch_size if batch_size is None else self.batch_size
-        batch_size = len(processed_prompts) if batch_size < 1 else batch_size
 
         answers = []
-        for i in range(0, len(processed_prompts), batch_size):
+        for i in range(0, len(processed_prompts)):
             logging.info(f"Running prompts {i}, to {i+1}")
             batch_prompts = [
                 self.generate_prompt(x)
