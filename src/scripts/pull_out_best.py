8871e2b309778030a7f34c0d4e1e99378e0216ea
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-25 18:02:32 -0800

    add script to get best structures for adslab

--- src/scripts/pull_out_best.py
+++ src/scripts/pull_out_best.py
@@ -1,5 +1,10 @@
 """Pull out the best structures."""
-import pathlib as path
+import json
+import shutil
+
+from pathlib import Path
+
+gnn_calcs_path = "/qfs/projects/aqe_tec4/catalysis/chemreasoner/catalysis/"
 
 catalysts = ["CuZnAl", "CuNiZn", "CuFeZn", "NiZnAl", "FeCoZn"]
 adsorbates = ["CO2", "*OCHO", "CHOH", "*OHCH3"]
@@ -7,4 +12,16 @@ adsorbates = ["CO2", "*OCHO", "CHOH", "*OHCH3"]
 for cat in catalysts:
     for ads in adsorbates:
         adslab_string = f"{cat}_{ads}"
+        adslab_path = Path(gnn_calcs_path, adslab_string)
+        json_path = adslab_path / "adsorption.json"
+        with open(json_path, "r") as f:
+            data = json.load(f)
+
+        min_idx = min(list(data.keys()), key=lambda k: data[k]["adsorption_energy"])
+
+        traj_path = list(adslab_path.rglob(f"{min_idx}-*.traj"))[0]
+
         print(adslab_string)
+        shutil.copy(
+            traj_path, Path("/people/spru445/methanol_results") / traj_path.stem
+        )
