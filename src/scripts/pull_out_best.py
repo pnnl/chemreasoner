dd37e421d6725e984a74fb27728d8f12372fc96d
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-25 19:08:54 -0800

    add escape character

--- src/scripts/pull_out_best.py
+++ src/scripts/pull_out_best.py
@@ -14,24 +14,21 @@ for cat in catalysts:
         cat = cat[0:2] + cat[4:] + cat[2:4]
 
     for ads in adsorbates:
-        try:
-            adslab_string = f"{cat}_{ads}"
-            adslab_path = Path(gnn_calcs_path, adslab_string)
-            # print(adslab_path.exists())
-            json_path = adslab_path / "adsorption.json"
-            with open(json_path, "r") as f:
-                data = json.load(f)
+        adslab_string = f"{cat}_{ads}"
+        adslab_path = Path(gnn_calcs_path, adslab_string)
+        # print(adslab_path.exists())
+        json_path = adslab_path / "adsorption.json"
+        with open(json_path, "r") as f:
+            data = json.load(f)
 
-            min_idx = min(list(data.keys()), key=lambda k: data[k]["adsorption_energy"])
+        min_idx = min(list(data.keys()), key=lambda k: data[k]["adsorption_energy"])
 
-            traj_path = list(adslab_path.rglob(f"{min_idx}-*.traj"))[0]
+        traj_path = list(adslab_path.rglob(f"{min_idx}-*.traj"))[0]
 
-            # print(adslab_string)
-            shutil.copy(
-                traj_path,
-                Path("/people/spru445/methanol_results")
-                / traj_path.parent.stem
-                / traj_path.stem,
-            )
-        except Exception:
-            print(f"skipping: {adslab_string}")
+        # print(adslab_string)
+        shutil.copy(
+            traj_path,
+            Path("/people/spru445/methanol_results")
+            / traj_path.parent.stem
+            / traj_path.stem,
+        )
