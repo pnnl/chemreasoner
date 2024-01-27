7ab2beb2c92553cd0929381763591bd22059f4d0
Choudhury, Sutanay <sutanay.choudhury@pnnl.gov>
2024-01-27 14:55:19 -0800

    Updated gnn_service.py

--- src/scripts/gnn_service.py
+++ src/scripts/gnn_service.py
@@ -5,6 +5,28 @@ from flask import Flask, request, render_template, jsonify, Response
 
 app = Flask(__name__)
 
+# TODO - initialize the StructureReward
+struct_reward_provider = StructureReward(TODO)
+
+def actual_gnn_func():
+    slab_syms = request.values.get("slab_syms")
+    ads_list = request.values.get("ads_list")
+    candidates_list = request.values.get("candidates_list")
+
+    adslabs_and_energies,
+                    gnn_calls,
+                    gnn_time,
+                    name_candidate_mapping = struct_reward_provider.create_structures_and_calculate(slab_syms,
+                ads_list, candidates_list)
+    output_dict = {
+        "adslabs_and_energies": adslabs_and_energies,
+        "gnn_calls": gnn_calls,
+        "gnn_time": gnn_time,
+        "name_candidate_mapping": name_candidate_mapping
+    }
+    response = jsonify(output_dict)
+    return response
+
 @app.route("/GemNet", methods=["POST"])
 def GemNet():
     #
