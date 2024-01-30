8886415e5478707bca5c7628682b1c8581dc1af3
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-29 17:53:49 -0800

    dump the data

--- src/scripts/parse_single_multi.py
+++ src/scripts/parse_single_multi.py
@@ -132,4 +132,4 @@ for p in results_files:
             except Exception as err:
                 print(err)
     with open(p.parent / (p.stem + "_rewards.json"), "w") as f:
-        data = json.load(f)
+        json.dump(data, f)
