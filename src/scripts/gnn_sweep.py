d0abbab82e61aa5e52b0b1a7eadf37fcd07e0685
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-01-31 14:38:22 -0800

    copy the data over

--- src/scripts/gnn_sweep.py
+++ src/scripts/gnn_sweep.py
@@ -1,5 +1,6 @@
 """Run the sweep over gnn calculated structures."""
 import random
+import shutil
 
 from pathlib import Path
 from tqdm import tqdm
@@ -16,4 +17,10 @@ while len(choices) < 40:
     if choice not in choices:
         ats = Trajectory(str(choice))[-1]
         if ats.get_potential_energy() < 10:
-            choices.append(str(choices))
+            print(choice)
+            choices.append(str(choice))
+            shutil.copy(
+                str(choice),
+                Path("gnn_eval_structures")
+                / (choice.parent.stem + "-" + choice.stem + ".traj"),
+            )
