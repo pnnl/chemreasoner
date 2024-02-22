ed64f2a4c8f89098ef1b3bf140f51c3b4561547b
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-02-22 12:06:52 -0800

    print pbc

--- src/nnp/oc.py
+++ src/nnp/oc.py
@@ -336,6 +336,7 @@ class OCAdsorptionCalculator(BaseAdsorptionCalculator):
         batch = Batch.from_data_list(
             self.ats_to_graphs.convert_all(bulk_atoms, disable_tqdm=True)
         )
+        print(self.ats_to_graphs.convert_all(bulk_atoms, disable_tqdm=True)[0].pbc)
         # device='cpu'
         batch = batch.to(device if device is not None else self.device)
 
