f5ac64465801e12eca68da7c057b608e37339c11
henry.sprueill@pnnl.gov <henry.sprueill@pnnl.gov>
2024-02-22 10:41:17 -0800

    print count of keys in data batch

--- src/nnp/oc.py
+++ src/nnp/oc.py
@@ -267,6 +267,12 @@ class OCAdsorptionCalculator(BaseAdsorptionCalculator):
             self.ats_to_graphs.convert_all(atoms, disable_tqdm=True)
         )
         print(type(self.ats_to_graphs.convert_all(atoms, disable_tqdm=True)[0]))
+        key_counts = {}
+        for data in self.ats_to_graphs.convert_all(atoms, disable_tqdm=True)[0]:
+            data_dict = data.to_dict()
+            for k, v in data_dict.items():
+                key_counts = 0 if k not in key_counts.keys() else key_counts[k] + 1
+        print(key_counts)
         batch.sid = atoms_names
         batch = batch.to(device if device is not None else self.device)
 
