66753e01ab4ad585e5665f6b299fa6170c015e1c
HenrySprueill <henry.sprueill@pnnl.gov>
2024-04-18 12:27:47 -0700

    add general utility for handling prompt processing

--- src/structure_creation/microsructure_planner.py
+++ src/structure_creation/microsructure_planner.py
@@ -1,8 +1,10 @@
-"""Moduel for microstructure creation from LLM answers."""
+"""Moduel for microstructure creation from LLM answers for the OCP format."""
 
 import logging
 import sys
 import time
+
+from typing import Optional
 
 sys.path.append("src")
 from search.state.reasoner_state import ReasonerState
@@ -10,12 +12,70 @@ from search.state.reasoner_state import ReasonerState
 logging.getLogger().setLevel(logging.INFO)
 
 
-class MicrostructurePlanner:
-    """A class to handle LLM-driven microstructure creation."""
+class OCPMicrostructurePlanner:
+    """A class to handle LLM-driven microstructure creation using the OCP format."""
+
+    default_retries = {
+        "symbols": 2,
+        "bulk": 0,
+        "millers": 0,
+        "cell_shift": 0,
+        "adsorbate_site": 0,
+        "__default__": 1,
+    }
 
     def __init__(self, llm_function=callable):
         """Init self."""
         self.llm_function = llm_function
+
+    def process_prompt(
+        self,
+        prompt_info: list[str],
+        prompt_type,
+        prompt_creation_function: callable[str, str | None],
+        prompt_parsing_function: callable[str, str | None],
+        system_prompt_function: callable[str, str | None],
+        retries: Optional[int] = None,
+        **llm_function_kwargs,
+    ):
+        """A script to handle the creation, running, parsing of a prompt."""
+        return_value = False
+        if isinstance(prompt_info, list):
+            return_value = True
+            prompt_info = [prompt_info]
+        if retries is None:
+            if prompt_type in self.default_retries:
+                retries = self.default_retries[prompt_type]
+            else:
+                retries = self.default_retries["__default__"]
+
+        attempts = 0
+        answers = [None] * len(prompt_info)
+        while attempts <= retries and any([ans is None for ans in answers]):
+            # Get the prompts
+            prompt_idx = []
+            prompts = []
+            system_prompts = []
+            for i, p_info in prompt_info:
+                if answers[i] is None:  # If answer hasn't been found, yet
+                    proposed_prompt = prompt_creation_function(p_info)
+                    if proposed_prompt is not None:  # If prompt creation is successful
+                        prompt_idx.append(i)
+                        prompts.append(prompt_creation_function(p_info))
+                        system_prompts.append(system_prompt_function(p_info))
+
+            # Run non-None prompts through the LLM to get raw answers
+            raw_answers = self.llm_function(
+                prompts, system_prompts, **llm_function_kwargs
+            )
+            for p_idx, raw_answer in zip(prompt_idx, raw_answers):
+                # Attempt to parse out the answer
+                answers[p_idx] = prompt_parsing_function(raw_answer)
+
+        if return_value:
+            return answers[0]
+        else:
+            return answers
 
     def run_generation_prompts(
         self, slab_syms: list[list[str]], states: list[ReasonerState]
@@ -118,13 +178,13 @@ class MicrostructurePlanner:
 
 example_data_structure = {
     {
-        "Answer": "(3) Zinc Oxide: This catalyst is good because...",
+        "llm_answer": "(3) Zinc Oxide: This catalyst is good because...",
         "catalyst_name": "Zinc Oxide",
         "symbols": ["Zn", "O"],
         "bulk_structure": "mp-123",
         "millers": (1, 2, 3),
         "cell_shift": 3,
         "site_placement": (3.2, 4.5, 6.7),
-        "atoms_objeect_id": "{db_id}_{structure_id}",
+        "atoms_object_id": "{db_id}_{structure_id}",
     }
 }
