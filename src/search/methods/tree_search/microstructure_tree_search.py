2037d16337b84e77ad6cccc153c3dc5ab5bda429
HenrySprueill <henry.sprueill@pnnl.gov>
2024-06-13 12:23:49 -0700

    add colorbar

--- src/search/methods/tree_search/microstructure_tree_search.py
+++ src/search/methods/tree_search/microstructure_tree_search.py
@@ -301,6 +301,7 @@ if __name__ == "__main__":
 
     visualize_tree(tree=tree)
     plt.title("**Placeholder values for rewards and catalyst values**")
+    plt.colorbar()
     plt.gcf().set_size_inches(18.5, 10.5)
     plt.savefig("test_tree.png", dpi=300)
     plt.show()
