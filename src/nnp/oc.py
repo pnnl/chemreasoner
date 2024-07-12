eb90542ab521136e75ac965fa240f8305e0f4802
HenrySprueill <henry.sprueill@pnnl.gov>
2024-07-12 12:05:26 -0700

    do 260 structures

--- src/nnp/oc.py
+++ src/nnp/oc.py
@@ -855,7 +855,7 @@ if __name__ == "__main__":
             )
         )
     )
-    example_structures = [example_structure.copy() for _ in range(257)]
+    example_structures = [example_structure.copy() for _ in range(260)]
 
     calc = OCAdsorptionCalculator(
         **{
