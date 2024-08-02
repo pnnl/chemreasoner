1bb904e913d4418d7d556620b7cb99ca150fa850
HenrySprueill <henry.sprueill@pnnl.gov>
2024-08-01 18:07:12 -0700

    add script to check adsorbed structures

---
+++ src/structure_creation/structure_checker.py
@@ -0,0 +1,57 @@
+"""Utilities for atomistic structures."""
+
+from pathlib import Path
+
+import numpy as np
+from ase.calculators.singlepoint import SinglePointCalculator
+
+from ase import Atoms
+from ase.io import Trajectory
+
+from ocdata.utils.flag_anomaly import DetectTrajAnomaly
+
+from tqdm import tqdm
+
+
+if __name__ == "__main__":
+    xyz_directory = Path("../cu_zn_test/trajectories")
+    print(xyz_directory)
+    traj_files = list(xyz_directory.rglob("*.traj"))
+
+    bad_counter = 0
+    good_counter = 0
+    for f in tqdm(traj_files):
+        traj = Trajectory(str(f))
+        anomaly_detector = DetectTrajAnomaly(
+            init_atoms=traj[0],
+            final_atoms=traj[-1],
+            atoms_tag=traj[0].get_tags(),
+        )
+        if anomaly_detector.has_surface_changed() or (
+            2 in traj[0].get_tags()
+            and any(
+                [
+                    (
+                        anomaly_detector.is_adsorbate_dissociated()  # adsorbate is dissociated
+                    ),
+                    (
+                        anomaly_detector.is_adsorbate_desorbed()  # flying off the surfgace
+                    ),
+                    (
+                        anomaly_detector.is_adsorbate_intercalated()  # interacting with bulk atom
+                    ),
+                ]
+            )
+        ):
+            bad_counter += 1
+            ats = traj[0].copy()
+            res = traj[0].calc.results
+            res["energy"] = np.nan
+            ats.calc = SinglePointCalculator(traj[0], **res)
+            print(ats.get_potential_energy())
+        else:
+            good_counter += 1 if 2 in traj[0].get_tags() else 0
+
+print(
+    f"bad_trajectories: {bad_counter}, good_trajectories: {good_counter} with {bad_counter + good_counter} total structures"
+)
