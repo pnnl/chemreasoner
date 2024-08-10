034d77797e26ee3a62efaa5fc098d3d3326e2ffe
HenrySprueill <henry.sprueill@pnnl.gov>
2024-08-10 15:44:59 -0700

    remove the parent directory

--- src/nnp/oc.py
+++ src/nnp/oc.py
@@ -1031,7 +1031,9 @@ if __name__ == "__main__":
             )[0]
 
             end = time.time()
-            torch.save(b.hessian, str(p.parent.parent / "hessians" / (p.stem + ".pt")))
+            torch.save(
+                b.hessian, str(p.parent.parent.parent / "hessians" / (p.stem + ".pt"))
+            )
             torch.save(
                 torch.Tensor([end - start]),
                 str(p.parent.parent / "hessians"(p.stem + "cpu_time.pt")),
