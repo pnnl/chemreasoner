7b7c9af86e3882e067fb097d48f57a32d785ca64
HenrySprueill <henry.sprueill@pnnl.gov>
2024-08-16 12:09:02 -0700

    use batch size 30 in uncertainty calc

--- src/nnp/uncertainty_prediction.py
+++ src/nnp/uncertainty_prediction.py
@@ -58,7 +58,7 @@ class UncertaintyCalculator:
             d.sid = atoms_names[i] if atoms_names is not None else None
         # convert to torch geometric batch
         uncertainties = []
-        dl = DataListLoader(data_list, batch_size=10000, shuffle=False)
+        dl = DataListLoader(data_list, batch_size=30, shuffle=False)
         for data_list in dl:
             batch = Batch.from_data_list(data_list)
             batch = batch.to("cpu")
