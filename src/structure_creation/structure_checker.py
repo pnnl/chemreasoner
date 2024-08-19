dec764d6dd54b5fdf66a489a1405f952a4cfa725
HenrySprueill <henry.sprueill@pnnl.gov>
2024-08-19 09:36:22 -0700

    store initial, final structure, dft structure

--- src/structure_creation/structure_checker.py
+++ src/structure_creation/structure_checker.py
@@ -15,11 +15,16 @@ from ocdata.utils.flag_anomaly import DetectTrajAnomaly
 from tqdm import tqdm
 
 if __name__ == "__main__":
-    save_dir = Path("cu_zn_dft_structures")
+    save_dir = Path("cu_zn_xyz_files")
+
+    dft_structure_dir = save_dir / "dft_structures"
+    initial_structure_dir = save_dir / "intial_structures"
+    final_structure_dir = save_dir / "final_structures"
+
     save_dir.mkdir(exist_ok=True)
-    xyz_directory = Path("cu_zn_with_H/trajectories")
-    print(xyz_directory)
-    traj_files = list(xyz_directory.rglob("*.traj"))
+    traj_directory = Path("cu_zn_with_H/trajectories")
+    print(traj_directory)
+    traj_files = list(traj_directory.rglob("*.traj"))
 
     codes = {}
     code_counts = {}
@@ -27,20 +32,31 @@ if __name__ == "__main__":
     good_counter = 0
     for f in tqdm(traj_files):
         traj = Trajectory(str(f))
+        initial_structure = traj[0]
+        final_structure = traj[-1]
         anomaly_detector = DetectTrajAnomaly(
-            init_atoms=traj[0],
-            final_atoms=traj[-1],
-            atoms_tag=traj[0].get_tags(),
+            init_atoms=initial_structure,
+            final_atoms=final_structure,
+            atoms_tag=initial_structure.get_tags(),
         )
-        fmax = np.max(np.sqrt(np.sum(traj[-1].get_forces() ** 2, axis=1)))
+        fmax = np.max(np.sqrt(np.sum(final_structure.get_forces() ** 2, axis=1)))
         code = None
         if anomaly_detector.has_surface_changed():
             code = 3
-        elif 2 in traj[0].get_tags() and anomaly_detector.is_adsorbate_dissociated():
+        elif (
+            2 in initial_structure.get_tags()
+            and anomaly_detector.is_adsorbate_dissociated()
+        ):
             code = 1
-        elif 2 in traj[0].get_tags() and anomaly_detector.is_adsorbate_desorbed():
+        elif (
+            2 in initial_structure.get_tags()
+            and anomaly_detector.is_adsorbate_desorbed()
+        ):
             code = 2
-        elif 2 in traj[0].get_tags() and anomaly_detector.is_adsorbate_intercalated():
+        elif (
+            2 in initial_structure.get_tags()
+            and anomaly_detector.is_adsorbate_intercalated()
+        ):
             code = 5
         elif fmax > 0.03:
             code = 6
@@ -55,7 +71,7 @@ if __name__ == "__main__":
             anomaly_detector.has_surface_changed()
             or fmax > 0.03
             or (
-                2 in traj[0].get_tags()
+                2 in initial_structure.get_tags()
                 and any(
                     [
                         (
@@ -75,9 +91,24 @@ if __name__ == "__main__":
         else:
 
             good_counter += 1
-        xyz_path = save_dir / (f.parent.stem) / (f.stem + ".xyz")
-        xyz_path.parent.mkdir(parents=True, exist_ok=True)
-        write(str(xyz_path), traj[-1])
+        final_path = final_structure_dir / (f.parent.stem) / (f.stem + ".xyz")
+        final_path.parent.mkdir(parents=True, exist_ok=True)
+        write(str(final_path), final_structure)
+
+        initial_path = initial_structure_dir / (f.parent.stem) / (f.stem + ".xyz")
+        initial_path.parent.mkdir(parents=True, exist_ok=True)
+        write(str(initial_path), initial_structure)
+
+        if code != 0 and code != 6:
+            dft_path = dft_structure_dir / (f.parent.stem) / (f.stem + ".xyz")
+            dft_path.parent.mkdir(parents=True, exist_ok=True)
+            write(str(dft_path), initial_structure)
+        else:
+            dft_path = dft_structure_dir / (f.parent.stem) / (f.stem + ".xyz")
+            dft_path.parent.mkdir(parents=True, exist_ok=True)
+            write(str(dft_path), final_structure)
+
+
 with open("convergence_error_codes.json", "w") as f:
     json.dump(codes, f)
 
