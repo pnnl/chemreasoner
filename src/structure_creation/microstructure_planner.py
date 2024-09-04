70e9be1e8753885524f5af25fb782b1ee024646d
HenrySprueill <henry.sprueill@pnnl.gov>
2024-09-04 16:41:54 -0700

    restart once

--- src/structure_creation/microstructure_planner.py
+++ src/structure_creation/microstructure_planner.py
@@ -81,9 +81,9 @@ class OCPMicrostructurePlanner:
 
     default_retries = {
         "symbols": 2,
-        "bulk": 0,
-        "millers": 0,
-        "site_placement": 0,
+        "bulk": 1,
+        "millers": 1,
+        "site_placement": 1,
         "__default__": 1,
     }
     num_choices = {
